0842a04436843633d3abe29409554599760a4871
hopefull-programmer <lbalsakaf@gmail.com>
2024-11-06 02:15:06 +0300

    project files

--- search.js
+++ search.js
@@ -1,38 +1,39 @@
-     const Products = [
-        { id: 1, name: 'ps5', oldPrice: 100, newPrice: 80, imageUrl: 'images/pic/pic.png', discount: 20 , category: "GameConsoles" },
-        { id: 2, name: 'ps5 pro', oldPrice: 150, newPrice: 112.50, imageUrl: 'images/pic/pro.png', discount: 25 , category: "GameConsoles"  },
-        { id: 3, name: 'ps5 slim', oldPrice: 90, newPrice: 70, imageUrl: 'images/pic/slim.png', discount: 22.22 , category: 'GameConsoles' },
-        { id: 4, name: 'steam deck', oldPrice: 200, newPrice: 160, imageUrl: 'images/pic/steam.png', discount: 20 , category: 'GameConsoles' },
-        { id: 5, name: 'nintendo switch', oldPrice: 75, newPrice: 60, imageUrl: 'images/pic/nin.png', discount: 20 , category: 'GameConsoles' },
-        { id: 6, name: 'xbox seris x', oldPrice: 120, newPrice: 90, imageUrl: 'images/pic/xbox.png', discount: 25 , category: 'GameConsoles' },
-        { id: 7, name: 'atari', oldPrice: 55, newPrice: 50, imageUrl: 'images/pic/nin.png', discount: 9.09 , category: 'GameConsoles' },
-        { id: 8, name: 'gameboy', oldPrice: 300, newPrice: 240, imageUrl: 'images/pic/gameboy.png', discount: 20 , category: 'GameConsoles' },
-        { id: 9, name: 'Psp', oldPrice: 85, newPrice: 65, imageUrl: 'images/pic/psp.png', discount: 23.53 , category: 'GameConsoles' },
-        { id: 10, name: 'playstation 4', oldPrice: 110, newPrice: 88, imageUrl: 'images/pic/ps4.png', discount: 20 , category: 'GameConsoles' },
-  { id: 11, name: 'ps5', oldPrice: 100, newPrice: 80, imageUrl: 'images/pic/11.png', discount: 20 , category: 'Cameras' },
-  { id: 12, name: 'ps5 pro', oldPrice: 150, newPrice: 112.50, imageUrl: 'images/pic/12.png', discount: 25 , category: 'Cameras' },
-  { id: 13, name: 'ps5 slim', oldPrice: 90, newPrice: 70, imageUrl: 'images/pic/13.png', discount: 22.22 , category: 'Cameras' },
-  { id: 14, name: 'steam deck', oldPrice: 200, newPrice: 160, imageUrl: 'images/pic/14.png', discount: 20 , category: 'Cameras' },
-  { id: 15, name: 'nintendo switch', oldPrice: 75, newPrice: 60, imageUrl: 'images/pic/15.png', discount: 20 , category: 'Cameras' },
-  { id: 16, name: 'ps5', oldPrice: 100, newPrice: 80, imageUrl: 'images/pic/1.png', discount: 20 , category: 'Home Appliances' },
-  { id: 17, name: 'ps5 pro', oldPrice: 150, newPrice: 112.50, imageUrl: 'images/pic/2.png', discount: 25 , category: 'Home Appliances' },
-  { id: 18, name: 'ps5 slim', oldPrice: 90, newPrice: 70, imageUrl: 'images/pic/3.png', discount: 22.22 , category: 'Home Appliances' },
-  { id: 19, name: 'steam deck', oldPrice: 200, newPrice: 160, imageUrl: 'images/pic/4.png', discount: 20 , category: 'Home Appliances' },
-  { id: 20, name: 'nintendo switch', oldPrice: 75, newPrice: 60, imageUrl: 'images/pic/5.png', discount: 20 , category: 'Home Appliances' },
-  { id: 21, name: 'xbox seris x', oldPrice: 120, newPrice: 90, imageUrl: 'images/pic/6.png', discount: 25 , category: 'Home Appliances' },
-  { id: 22, name: 'atari', oldPrice: 55, newPrice: 50, imageUrl: 'images/pic/7.png', discount: 9.09 , category: 'Home Appliances' },
-  { id: 23, name: 'gameboy', oldPrice: 300, newPrice: 240, imageUrl: 'images/pic/8.png', discount: 20 , category: 'Home Appliances' },
-  { id: 24, name: 'Psp', oldPrice: 85, newPrice: 65, imageUrl: 'images/pic/9.png', discount: 23.53 , category: 'AHome Appliances' },
-  { id: 25, name: 'playstation 4', oldPrice: 110, newPrice: 88, imageUrl: 'images/pic/10.png', discount: 20 , category: 'Home Appliances' },
-  { id: 26, name: 'ps5 pro', oldPrice: 150, newPrice: 112.50, imageUrl: 'images/pic/17.png', discount: 25 , category: 'TV' },
-  { id: 27, name: 'ps5 slim', oldPrice: 90, newPrice: 70, imageUrl: 'images/pic/18.png', discount: 22.22 , category: 'TV' },
-  { id: 28, name: 'steam deck', oldPrice: 200, newPrice: 160, imageUrl: 'images/pic/19.png', discount: 20 , category: 'TV' },
-  { id: 29, name: 'nintendo switch', oldPrice: 75, newPrice: 60, imageUrl: 'images/pic/20.png', discount: 20 , category: 'TV' },
-  { id: 30, name: 'xbox seris x', oldPrice: 120, newPrice: 90, imageUrl: 'images/pic/21.png', discount: 25 , category: 'TV' },
-  { id: 31, name: 'atari', oldPrice: 55, newPrice: 50, imageUrl: 'images/pic/22.png', discount: 9.09 , category: 'TV' },
-  { id: 32, name: 'gameboy', oldPrice: 300, newPrice: 240, imageUrl: 'images/pic/23.png', discount: 20 , category: 'TV' },
-        ];
-
+const Products = [
+    { id: 1, name: 'ps5', oldPrice: 100, newPrice: 80, imageUrl: 'images/pic/pic.png', discount: 20 },
+{ id: 2, name: 'ps5 pro', oldPrice: 150, newPrice: 112.50, imageUrl: 'images/pic/pro.png', discount: 25 },
+{ id: 3, name: 'ps5 slim', oldPrice: 90, newPrice: 70, imageUrl: 'images/pic/slim.png', discount: 22.22 },
+{ id: 4, name: 'steam deck', oldPrice: 200, newPrice: 160, imageUrl: 'images/pic/steam.png', discount: 20 },
+{ id: 5, name: 'nintendo switch', oldPrice: 75, newPrice: 60, imageUrl: 'images/pic/nin.png', discount: 20 },
+{ id: 6, name: 'xbox seris x', oldPrice: 120, newPrice: 90, imageUrl: 'images/pic/xbox.png', discount: 25 },
+{ id: 7, name: 'atari', oldPrice: 55, newPrice: 50, imageUrl: 'images/pic/nin.png', discount: 9.09 },
+{ id: 8, name: 'gameboy', oldPrice: 300, newPrice: 240, imageUrl: 'images/pic/gameboy.png', discount: 20 },
+{ id: 9, name: 'Psp', oldPrice: 85, newPrice: 65, imageUrl: 'images/pic/psp.png', discount: 23.53 },
+{ id: 10, name: 'playstation 4', oldPrice: 110, newPrice: 88, imageUrl: 'images/pic/ps4.png', discount: 20 },
+{ id: 11, name: 'ps5', oldPrice: 100, newPrice: 80, imageUrl: 'images/pic/11.png', discount: 20 },
+{ id: 12, name: 'ps5 pro', oldPrice: 150, newPrice: 112.50, imageUrl: 'images/pic/12.png', discount: 25 },
+{ id: 13, name: 'ps5 slim', oldPrice: 90, newPrice: 70, imageUrl: 'images/pic/13.png', discount: 22.22 },
+{ id: 14, name: 'steam deck', oldPrice: 200, newPrice: 160, imageUrl: 'images/pic/14.png', discount: 20 },
+{ id: 15, name: 'nintendo switch', oldPrice: 75, newPrice: 60, imageUrl: 'images/pic/15.png', discount: 20 },
+{ id: 16, name: 'nintendo switch', oldPrice: 77, newPrice: 60, imageUrl: 'images/pic/16.png', discount: 20 },
+{ id: 17, name: 'nintendo switch', oldPrice: 77, newPrice: 60, imageUrl: 'images/pic/17-c.png', discount: 20 },
+{ id: 18, name: 'ps5', oldPrice: 100, newPrice: 80, imageUrl: 'images/pic/1.png', discount: 20 },
+{ id: 19, name: 'ps5 pro', oldPrice: 150, newPrice: 112.50, imageUrl: 'images/pic/2.png', discount: 25 },
+{ id: 20, name: 'ps5 slim', oldPrice: 90, newPrice: 70, imageUrl: 'images/pic/3.png', discount: 22.22 },
+{ id: 21, name: 'steam deck', oldPrice: 200, newPrice: 160, imageUrl: 'images/pic/4.png', discount: 20 },
+{ id: 22, name: 'nintendo switch', oldPrice: 75, newPrice: 60, imageUrl: 'images/pic/5.png', discount: 20 },
+{ id: 23, name: 'xbox seris x', oldPrice: 120, newPrice: 90, imageUrl: 'images/pic/6.png', discount: 25 },
+{ id: 24, name: 'atari', oldPrice: 55, newPrice: 50, imageUrl: 'images/pic/7.png', discount: 9.09 },
+{ id: 25, name: 'gameboy', oldPrice: 300, newPrice: 240, imageUrl: 'images/pic/8.png', discount: 20 },
+{ id: 26, name: 'Psp', oldPrice: 85, newPrice: 65, imageUrl: 'images/pic/9.png', discount: 23.53 },
+{ id: 27, name: 'playstation 4', oldPrice: 110, newPrice: 88, imageUrl: 'images/pic/10.png', discount: 20 },
+{ id: 28, name: 'ps5 pro', oldPrice: 150, newPrice: 112.50, imageUrl: 'images/pic/17.png', discount: 25 },
+{ id: 29, name: 'ps5 slim', oldPrice: 90, newPrice: 70, imageUrl: 'images/pic/18.png', discount: 22.22 },
+{ id: 30, name: 'steam deck', oldPrice: 200, newPrice: 160, imageUrl: 'images/pic/19.png', discount: 20 },
+{ id: 31, name: 'nintendo switch', oldPrice: 75, newPrice: 60, imageUrl: 'images/pic/20.png', discount: 20 },
+{ id: 32, name: 'xbox seris x', oldPrice: 120, newPrice: 90, imageUrl: 'images/pic/21.png', discount: 25 },
+{ id: 33, name: 'atari', oldPrice: 55, newPrice: 50, imageUrl: 'images/pic/22.png', discount: 9.09 },
+{ id: 34, name: 'gameboy', oldPrice: 300, newPrice: 240, imageUrl: 'images/pic/23.png', discount: 20 },
+    ];
 
 function getQueryParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
@@ -48,7 +49,6 @@ function displaySearchResults(filteredProducts) {
         filteredProducts.forEach(product => {
             const productCard = document.createElement('div');
             productCard.classList.add('product-card'); // Add a class for styling
-
             productCard.innerHTML = `
                 <a href="product-detail.html?id=${product.id}">
                     <img src="${product.imageUrl}" alt="Product ${product.id}" class="product-image" style="width: 150px;">
@@ -67,6 +67,8 @@ function displaySearchResults(filteredProducts) {
             `;
             resultsContainer.appendChild(productCard);
         });
+
+        
     } else {
         resultsTitle.textContent = 'No results found.';
     }
